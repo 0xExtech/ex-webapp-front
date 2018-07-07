4bd0bd0062adfbb3ed45361451a33806cb97c557
Timothé MAILHAC <timothe.mailhac@gmail.com>
2018-07-07 22:45:32 +0200

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,5 @@
+<template src="./Home/Home.html"></template>
+
+<script src="./Home/Home.js"></script>
+
+<style scoped src="./Home/Home.css"></style>
---
+++ src/components/Home/Home.js
@@ -0,0 +1,8 @@
+export default {
+    name: "Home",
+    data() {
+        return {
+            msg: 'Welcome to the EX-WEBAPP!'
+        }
+    }
+}
